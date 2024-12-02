cd939058c90ffcbe56f7954b70ba638b5b45860a
Obinna Anosike <smartlify09@gmail.com>
2024-12-03 00:56:17 +0100

    fix

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -4,8 +4,8 @@ import logo from '/public/images/logo.svg';
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <nav className="py-5 px-4  rounded-xl flex items-center justify-center font-raleway">
-      <div className="w-11/12 flex items-center justify-between">
+    <nav className="py-5 px-6 md:px-4  rounded-xl flex items-center justify-center font-raleway">
+      <div className="w-full md:w-11/12 flex items-center justify-between">
         <Link
           to={'/'}
           className="text-warmorange font-medium font-playfair italic text-4xl "
@@ -14,7 +14,7 @@ const Navbar = () => {
           <span className="hidden md:block">Thankful</span>
         </Link>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-4 md:gap-2">
           <button
             onClick={() => navigate('/signin')}
             className="py-2 px-3 rounded-lg md:rounded-full w-auto lg:w-[120px] md:h-[48px] border-[1.5px] text-slate-900 font-medium"
