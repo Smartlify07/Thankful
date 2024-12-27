2b50c7850aa425fac5f0d5002b0be17262571b9f
Obinna Anosike <smartlify09@gmail.com>
2024-12-27 22:13:19 +0100

    homepage feature section

--- src/constants.ts
+++ src/constants.ts
@@ -3,32 +3,28 @@ export const featuresData = [
     header: 'Create Meaningful Messages in Seconds',
     description:
       'Craft heartfelt thank-you messages effortlessly. Our intuitive platform helps you express gratitude with clarity and ease, so you can focus on making someone’s day brighter.',
-    image: '', // Image placeholder for now
-    icon: '', // Icon placeholder for now
+    icon: '⚡💌',
     highlightableTexts: ['heartfelt', 'gratitude', 'brighter'],
   },
   {
     header: 'Your Memories, Organized',
     description:
       'Revisit your most cherished moments anytime. With our message library, every thank-you note is stored safely, ready for you to reflect on whenever you need.',
-    image: '', // Image placeholder for now
-    icon: '', // Icon placeholder for now
+    icon: '✨📖',
     highlightableTexts: ['cherished', 'library', 'reflect'],
   },
   {
     header: 'The Right Words, Every Time',
     description:
       'Struggling to find the right words? Let AI help you craft thoughtful, personalized messages that capture exactly how you feel.',
-    image: '', // Image placeholder for now
-    icon: '', // Icon placeholder for now
+    icon: '❣️📝',
     highlightableTexts: ['words', 'AI', 'thoughtful'],
   },
   {
     header: 'Make It Personal',
     description:
       'Add a unique touch to your thank-you notes with customizable designs, fonts, and colors. Your message, your way.',
-    image: '', // Image placeholder for now
-    icon: '', // Icon placeholder for now
+    icon: '🤗',
     highlightableTexts: ['unique', 'designs', 'message'],
   },
 ];
--- src/ui/Home/features.tsx
+++ src/ui/Home/features.tsx
@@ -5,7 +5,7 @@ const Features = () => {
   return (
     <section
       id="features"
-      className="flex flex-col  py-10 mt-10  px-20 justify-between items-start gap-5 font-rubikk"
+      className="flex flex-col  py-10 mt-10  px-10 justify-between items-start gap-7 font-inter"
     >
       {featuresData.map((item, index) => (
         <Feature index={index} key={item.header} {...item} />
