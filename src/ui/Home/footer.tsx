d893734acdcd4d96d4fe234ef7ab261b5922c951
Obinna Anosike <smartlify09@gmail.com>
2024-12-27 23:09:54 +0100

    build footer

--- src/ui/Home/footer.tsx
+++ src/ui/Home/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
   return (
     <footer className="py-10 flex bg-black items-center justify-center">
-      <div className="2xl:max-w-[1440px] flex flex-col items-center md:gap-7">
+      <div className="2xl:max-w-[1440px] flex flex-col items-center gap-5 md:gap-7">
         <header>
           <h1 className="text-lime text-center text-5xl font-playfair-display italic tracking-tight md:text-7xl">
             Thankful
