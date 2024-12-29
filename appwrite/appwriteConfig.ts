8b5c57ed04a4eb3ad6bb54de855fd1525aa2df8e
Obinna Anosike <smartlify09@gmail.com>
2024-12-29 19:53:45 +0100

    remove clgs

--- appwrite/appwriteConfig.ts
+++ appwrite/appwriteConfig.ts
@@ -8,9 +8,4 @@ client
 export const account: Account = new Account(client);
 export const database: Databases = new Databases(client);
 
-console.log(
-  import.meta.env.VITE_APP_API_URL!,
-  import.meta.env.VITE_APP_PROJECT_ID!
-);
-
 export { ID } from 'appwrite';
