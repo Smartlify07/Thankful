a317bb2efd770b36a8dc15547add695454a42fac
Obinna Anosike <smartlify09@gmail.com>
2025-01-03 21:52:49 +0100

    update stuff

--- src/ui/(library)/message.tsx
+++ src/ui/(library)/message.tsx
@@ -1,7 +1,7 @@
 import type { Message } from '@/types/types';
 import { getRandomColor } from '@/utils/getRandomColor';
 import { lightenColor } from '@/utils/lightenColor';
-import { motion } from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
 import { useState } from 'react';
 
 import MessagePopout from './message-popout';
@@ -28,27 +28,27 @@ const Message = ({ content, title, $id }: Message) => {
           boxShadow: 'rgba(0, 0, 0, 0.1) 0px 10px 20px -1px',
           scale: 1.02,
         }}
-        className="flex flex-col relative items-start overflow-hidden  px-3 py-5 gap-3 rounded-lg cursor-pointer shadow-md text-outer-space lg:col-span-1 "
+        className="flex flex-col relative items-start   px-3 py-5 gap-3 rounded-lg cursor-pointer shadow-md text-outer-space lg:col-span-1 "
       >
         <h1
           className={`top-3 w-full text-lg sm:text-xl md:text-2xl font-raleway font-medium`}
         >
           {title}
         </h1>
-        <p className={`text-lg font-openSans h-[300px] overflow-hidden`}>
-          {content}
-        </p>
+        <p className={`text-lg font-openSans overflow-hidden`}>{content}</p>
       </motion.div>
 
-      {expand && (
-        <MessagePopout
-          title={title}
-          content={content}
-          expand={expand}
-          setExpand={setExpand}
-          $id={$id}
-        />
-      )}
+      <AnimatePresence mode="popLayout">
+        {expand && (
+          <MessagePopout
+            title={title}
+            content={content}
+            expand={expand}
+            setExpand={setExpand}
+            $id={$id}
+          />
+        )}
+      </AnimatePresence>
     </>
   );
 };
