9cac4359dd99a2112daff3e2f6a98ef4903e7b70
Obinna Anosike <smartlify09@gmail.com>
2025-01-04 12:03:18 +0100

    update components

--- src/ui/(library)/message-dropdown.tsx
+++ src/ui/(library)/message-dropdown.tsx
@@ -2,7 +2,7 @@ import { deleteMessage } from '@/redux/features/messages/messagesSlice';
 import { AppDispatch } from '@/redux/store';
 import { isErrorWithMessage } from '@/utils/isErrorWithMessage';
 import { motion } from 'motion/react';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, forwardRef, SetStateAction } from 'react';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 type MessageDropdownProps = {
@@ -10,48 +10,47 @@ type MessageDropdownProps = {
   setIsDropdownOpen: Dispatch<SetStateAction<boolean>>;
   setIsMessageOpen: Dispatch<SetStateAction<boolean>>;
 };
-const MessageDropdown = ({
-  $id,
-  setIsMessageOpen,
-  setIsDropdownOpen,
-}: MessageDropdownProps) => {
-  const dispatch: AppDispatch = useDispatch();
-  const handleDeleteMessage = async () => {
-    try {
-      await dispatch(deleteMessage($id)).unwrap();
-    } catch (error) {
-      let message;
-      if (isErrorWithMessage(error)) {
-        message = error.message;
+const MessageDropdown = forwardRef<HTMLDivElement, MessageDropdownProps>(
+  function MessageDropdown({ $id, setIsMessageOpen, setIsDropdownOpen }, ref) {
+    const dispatch: AppDispatch = useDispatch();
+    const handleDeleteMessage = async () => {
+      try {
+        await dispatch(deleteMessage($id)).unwrap();
+      } catch (error) {
+        let message;
+        if (isErrorWithMessage(error)) {
+          message = error.message;
+        }
+        console.error(error);
+        toast(message, {});
       }
-      console.error(error);
-      toast(message, {});
-    }
-  };
-  const handleOpenMessage = async () => {
-    setIsMessageOpen(true);
-  };
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.1, delay: 0 }}
-      className="bg-white rounded-md absolute shadow-lg border flex w-[110px] flex-col gap-0 py-3 -right-5 top-14"
-      onMouseLeave={() => {
-        setIsDropdownOpen(false);
-      }}
-    >
-      <Option label="Open" action={handleOpenMessage} />
-      <Option label="Share" />
-      <Option
-        label="Delete"
-        action={handleDeleteMessage}
-        className="text-red-500"
-      />
-    </motion.div>
-  );
-};
+    };
+    const handleOpenMessage = async () => {
+      setIsMessageOpen(true);
+    };
+    return (
+      <motion.div
+        ref={ref}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.1, delay: 0 }}
+        className="bg-white rounded-md absolute shadow-lg border flex w-[110px] flex-col gap-0 py-3 -right-5 top-14"
+        onMouseLeave={() => {
+          setIsDropdownOpen(false);
+        }}
+      >
+        <Option label="Open" action={handleOpenMessage} />
+        <Option label="Share" />
+        <Option
+          label="Delete"
+          action={handleDeleteMessage}
+          className="text-red-500"
+        />
+      </motion.div>
+    );
+  }
+);
 
 const Option = ({
   label,
--- src/ui/(library)/message-popout.tsx
+++ src/ui/(library)/message-popout.tsx
@@ -3,7 +3,7 @@ import { getRandomColor } from '@/utils/getRandomColor';
 import { lightenColor } from '@/utils/lightenColor';
 import { motion } from 'motion/react';
 import Button from '../button';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, forwardRef, SetStateAction, useState } from 'react';
 import { AppDispatch } from '@/redux/store';
 import { useDispatch } from 'react-redux';
 import { editMessage } from '@/redux/features/messages/messagesSlice';
@@ -14,95 +14,99 @@ type MessagePopoutProps = Message & {
   isMessageOpen: boolean;
   setIsMessageOpen: Dispatch<SetStateAction<boolean>>;
 };
-const MessagePopout = ({
-  title,
-  content,
-  isMessageOpen,
-  setIsMessageOpen,
-  $id,
-}: MessagePopoutProps) => {
-  const color = getRandomColor();
-  const lighterVersion = lightenColor(color, 60);
-  // @todo: add prop for color, to prevent change on open
+const MessagePopout = forwardRef<HTMLDivElement, MessagePopoutProps>(
+  function MessagePopout(
+    { isMessageOpen, setIsMessageOpen, title, content, $id },
+    ref
+  ) {
+    const color = getRandomColor();
+    const lighterVersion = lightenColor(color, 60);
+    // @todo: add prop for color, to prevent change on open
 
-  const [message, setMessage] = useState<Message>({
-    title: title,
-    content: content,
-  });
+    const [message, setMessage] = useState<Message>({
+      title: title,
+      content: content,
+    });
 
-  const dispatch: AppDispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch();
 
-  const updateMessage = async (title: string, content: string, $id: string) => {
-    await dispatch(
-      editMessage({
-        title,
-        content,
-        $id,
-      })
-    );
-  };
-  const debouncedUpdateMessage = useDebounce(updateMessage, 300);
-  return (
-    <motion.div
-      initial={{ backgroundColor: 'rgb(37,37,37,0.5)', opacity: 0 }}
-      animate={{ opacity: isMessageOpen ? 1 : 0 }}
-      exit={{
-        opacity: 0,
-      }}
-      className="w-full z-10 fixed
-   top-0 left-0 min-h-screen flex items-center justify-center"
-    >
+    const updateMessage = async (
+      title: string,
+      content: string,
+      $id: string
+    ) => {
+      await dispatch(
+        editMessage({
+          title,
+          content,
+          $id,
+        })
+      );
+    };
+    const debouncedUpdateMessage = useDebounce(updateMessage, 300);
+    return (
       <motion.div
-        initial={{
-          minHeight: '100vh',
-          backgroundColor: lighterVersion,
-          boxShadow:
-            'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.1) 0px 2px 4px -2px',
-          translateY: '100%',
-        }}
-        animate={{
-          translateY: '0%',
-        }}
-        transition={{
-          duration: 0.5,
-        }}
+        ref={ref}
+        initial={{ backgroundColor: 'rgb(37,37,37,0.5)', opacity: 0 }}
+        animate={{ opacity: isMessageOpen ? 1 : 0 }}
         exit={{
-          translateY: '100%',
+          opacity: 0,
         }}
-        className="flex flex-col relative w-8/12 items-start px-16 py-5 gap-10 shadow-md text-outer-space lg:col-span-1 2xl:max-w-[1320px]"
+        className="w-full z-10 fixed
+   top-0 left-0 min-h-screen flex items-center justify-center"
       >
-        <Button
-          onClick={() => setIsMessageOpen(false)}
-          className="rounded-full absolute flex items-center justify-center right-6 top-6"
+        <motion.div
+          initial={{
+            minHeight: '100vh',
+            backgroundColor: lighterVersion,
+            boxShadow:
+              'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.1) 0px 2px 4px -2px',
+            translateY: '100%',
+          }}
+          animate={{
+            translateY: '0%',
+          }}
+          transition={{
+            duration: 0.5,
+          }}
+          exit={{
+            translateY: '100%',
+          }}
+          className="flex flex-col relative w-8/12 items-start px-16 py-5 gap-10 shadow-md text-outer-space lg:col-span-1 2xl:max-w-[1320px]"
         >
-          <PiX size={24} className="text-charcoal-gray" />
-        </Button>{' '}
-        <input
-          placeholder="Title"
-          onChange={async (e) => {
-            setMessage((prevState) => ({
-              ...prevState,
-              title: e.target.value,
-            }));
-            debouncedUpdateMessage(e.target.value, message.content!, $id!);
-          }}
-          className={`top-3 w-full text-lg sm:text-xl md:text-3xl font-openSans font-medium text-center bg-transparent outline-none focus:outline-none focus:border-none placeholder:text-neutral-600`}
-          value={message.title}
-        />
-        <textarea
-          onChange={(e) => {
-            setMessage((prevState) => ({
-              ...prevState,
-              content: e.target.value,
-            }));
-            debouncedUpdateMessage(message.title!, e.target.value, $id!);
-          }}
-          value={message.content}
-          className={`text-xl bg-transparent w-full flex items-start font-openSans focus:border-none focus:outline-none min-h-[100vh] resize-none`}
-        />
+          <Button
+            onClick={() => setIsMessageOpen(false)}
+            className="rounded-full absolute flex items-center justify-center right-6 top-6"
+          >
+            <PiX size={24} className="text-charcoal-gray" />
+          </Button>{' '}
+          <input
+            placeholder="Title"
+            onChange={async (e) => {
+              setMessage((prevState) => ({
+                ...prevState,
+                title: e.target.value,
+              }));
+              debouncedUpdateMessage(e.target.value, message.content!, $id!);
+            }}
+            className={`top-3 w-full text-lg sm:text-xl md:text-3xl font-openSans font-medium text-center bg-transparent outline-none focus:outline-none focus:border-none placeholder:text-neutral-600`}
+            value={message.title}
+          />
+          <textarea
+            onChange={(e) => {
+              setMessage((prevState) => ({
+                ...prevState,
+                content: e.target.value,
+              }));
+              debouncedUpdateMessage(message.title!, e.target.value, $id!);
+            }}
+            value={message.content}
+            className={`text-xl bg-transparent w-full flex items-start font-openSans focus:border-none focus:outline-none min-h-[100vh] resize-none`}
+          />
+        </motion.div>
       </motion.div>
-    </motion.div>
-  );
-};
+    );
+  }
+);
 
 export default MessagePopout;
